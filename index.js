674d0e903a914630ebda6370f663646555252466
Abdullahlionn <80156629+Abdullalionn@users.noreply.github.com>
2024-05-12 21:38:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,85 +1,106 @@
-function addTodo() {
-  var todoInput = document.getElementById("todoInput");
-  var todoList = document.getElementById("todoList");
+const image = document.getElementById("cover"),
+  title = document.getElementById("music-title"),
+  artist = document.getElementById("music-artist"),
+  currentTimeEl = document.getElementById("current-time"),
+  durationEl = document.getElementById("duration"),
+  progress = document.getElementById("progress"),
+  playerProgress = document.getElementById("player-progress"),
+  prevBtn = document.getElementById("prev"),
+  nextBtn = document.getElementById("next"),
+  playBtn = document.getElementById("play"),
+  background = document.getElementById("bg-img");
 
-  if (todoInput.value.trim() === "") {
-    return;
-  }
+const music = new Audio();
 
-  var li = document.createElement("li");
-  li.textContent = todoInput.value;
+const songs = [
+  {
+    path: "https://abdullalionn.github.io/7OB%20KHENA2.mp3",
+    displayName: "7OB KHENA2",
+    cover: "https://abdullalionn.github.io/7OB%20KHENA2.png",
+    artist: "Marawan Moussa",
+  },
+  {
+    path: "https://abdullalionn.github.io/%D8%A8%D8%B7%D9%84%20%D8%B9%D8%A7%D9%84%D9%85.mp3",
+    displayName: "بطل عالم",
+    cover: "https://abdullalionn.github.io/batal%203alam.png",
+    artist: "Marawan Moussa",
+  },
+  {
+    path: "https://abdullalionn.github.io/Brazil.mp3",
+    displayName: "Brazil",
+    cover: "https://abdullalionn.github.io/brazil.png",
+    artist: "Afroto & Marawan Moussa",
+  },
+];
 
-  var div = document.createElement("div");
-  div.className = "button-group";
+let musicIndex = 0;
+let isPlaying = false;
 
-  var deleteButton = document.createElement("button");
-  deleteButton.innerHTML = '<i class="fas fa-ellipsis-v menu-icon"></i>';
-  deleteButton.classList.add("actions");
-
-  var dropdownContent = document.createElement("div");
-  dropdownContent.className = "dropdown-content";
-  var deleteOption = document.createElement("button");
-  deleteOption.innerHTML = "Delete";
-  deleteOption.onclick = function () {
-    li.remove();
-  };
-  var disableOption = document.createElement("button");
-  disableOption.innerHTML = "Disable";
-  disableOption.onclick = function () {
-    // Add your code for disabling here
-    alert("Task disabled!");
-  };
-
-  dropdownContent.appendChild(deleteOption);
-  dropdownContent.appendChild(disableOption);
-
-  div.appendChild(deleteButton);
-  div.appendChild(dropdownContent);
-
-  li.appendChild(div);
-  todoInput.value = "";
-  todoList.appendChild(li);
-}
-
-document
-  .getElementById("todoInput")
-  .addEventListener("keypress", function (event) {
-    // Check if Enter key is pressed
-    if (event.key === "Enter") {
-      // Log the text from the input to the console
-      console.log(this.value);
-      addTodo();
-    }
-  });
-// Get references to the button and pop-up widget
-const toggleButton = document.getElementById("toggleButton");
-const popupWidget = document.getElementById("popupWidget");
-const closeButton = document.getElementById("closeButton");
-var body = document.getElementById("body");
-
-// Function to toggle the visibility of the pop-up widget
-function togglePopup() {
-  if (popupWidget.style.display === "none") {
-    popupWidget.style.display = "block";
+function togglePlay() {
+  if (isPlaying) {
+    pauseMusic();
   } else {
-    popupWidget.style.display = "none";
+    playMusic();
   }
 }
 
-// Add event listeners to the toggle button and close button
-toggleButton.addEventListener("click", togglePopup);
-closeButton.addEventListener("click", togglePopup);
-
-var themebutton = document.getElementById("theme");
-
-function themechange() {
-  if (body.classList.contains("dark-theme")) {
-    body.classList.remove("dark-theme");
-    themebutton.textContent = "Light☀️";
-  } else {
-    body.classList.add("dark-theme");
-    themebutton.textContent = "Dark🌙";
-  }
+function playMusic() {
+  isPlaying = true;
+  // Change play button icon
+  playBtn.classList.replace("fa-play", "fa-pause");
+  // Set button hover title
+  playBtn.setAttribute("title", "Pause");
+  music.play();
 }
 
-themebutton.addEventListener("click", themechange);
+function pauseMusic() {
+  isPlaying = false;
+  // Change pause button icon
+  playBtn.classList.replace("fa-pause", "fa-play");
+  // Set button hover title
+  playBtn.setAttribute("title", "Play");
+  music.pause();
+}
+
+function loadMusic(song) {
+  music.src = song.path;
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  image.src = song.cover;
+  background.src = song.cover;
+}
+
+function changeMusic(direction) {
+  musicIndex = (musicIndex + direction + songs.length) % songs.length;
+  loadMusic(songs[musicIndex]);
+  playMusic();
+}
+
+function updateProgressBar() {
+  const { duration, currentTime } = music;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+
+  const formatTime = (time) => String(Math.floor(time)).padStart(2, "0");
+  durationEl.textContent = `${formatTime(duration / 60)}:${formatTime(
+    duration % 60
+  )}`;
+  currentTimeEl.textContent = `${formatTime(currentTime / 60)}:${formatTime(
+    currentTime % 60
+  )}`;
+}
+
+function setProgressBar(e) {
+  const width = playerProgress.clientWidth;
+  const clickX = e.offsetX;
+  music.currentTime = (clickX / width) * music.duration;
+}
+
+playBtn.addEventListener("click", togglePlay);
+prevBtn.addEventListener("click", () => changeMusic(-1));
+nextBtn.addEventListener("click", () => changeMusic(1));
+music.addEventListener("ended", () => changeMusic(1));
+music.addEventListener("timeupdate", updateProgressBar);
+playerProgress.addEventListener("click", setProgressBar);
+
+loadMusic(songs[musicIndex]);
